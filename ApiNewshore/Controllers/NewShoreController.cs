a215f18e78365f10e4991849c578985799603fb3
nealos8907 <72480543+nealos8907@users.noreply.github.com>
2022-05-22 13:19:32 -0500

    Add files via upload

---
+++ ApiNewshore/Controllers/NewShoreController.cs
@@ -0,0 +1,56 @@
+﻿using Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Repositories;
+using System;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiNewshore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewShoreController : ControllerBase
+    {
+        private readonly INewShoreRepository _newShoreRepository;
+        private readonly IHandleNewShoreDbRepository _handleNewShoreDbRepository;
+        private readonly ILogger<NewShoreController> _logger;
+        public NewShoreController(INewShoreRepository newShoreRepository, 
+            IHandleNewShoreDbRepository handleNewShoreDbRepository,
+            ILogger<NewShoreController> logger)
+        {
+            _newShoreRepository = newShoreRepository;
+            _handleNewShoreDbRepository = handleNewShoreDbRepository;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("GetRoute")]
+        public async Task<IActionResult>  GetRoute([FromBody] RequestJourneyDto requestJourney )
+        {
+            var result = new JourneyDto();
+            try
+            {
+                result = await _newShoreRepository.GetRoutesNewShore(requestJourney);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                
+            }
+            catch (System.Exception ex)
+            {
+
+                _logger.LogError($"Fecha:{DateTime.Now}- Error: {ex} -Parameters: {JsonConvert.SerializeObject(requestJourney)}");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            //Codigo 204
+            return BadRequest("VUELO NO ENCONTRADO");
+            
+        }
+
+    }
+}

b25d976d649a04dc5d8782b7037d7a0b5b1b1d5e
nealos8907 <72480543+nealos8907@users.noreply.github.com>
2022-05-22 13:20:40 -0500

    Add files via upload

---
+++ Domain/Journey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    
+    public class Journey
+    {
+        [Key]
+        public int JourneyId { get; set; }      
+
+        public string Origin { get; set; }
+
+        public string Destination { get; set; }
+
+        public double Price { get; set; }
+
+        public virtual ICollection<JourneyFlight> JourneyFlight { get; set; }
+    }
+}
---
+++ Domain/JourneyFlight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class JourneyFlight
+    { 
+        public int JourneyFlightId { get; set; }
+
+        public int JourneyId { get; set; }
+
+        public int FlightId { get; set; }
+
+        public virtual Journey Journey { get; set; }
+
+        public virtual Flight Flight { get; set; }
+    }
+}
